c352156a0004fa51e5bad63dc68261e146afd1a6
david <davidola36@yahoo.com>
2021-08-05 14:12:36 +0100

    added method to fetch all users and friends data from db and twitter

--- server/controllers/twitter.js
+++ server/controllers/twitter.js
@@ -1,10 +1,10 @@
 const twitterClient = require('../api/twitter')
 const async = require("async")
-
+const User = require("../models/user")
 
 class Twitter {
 
-    async getFriends(user_id, count) {
+    getFriends(user_id, count) {
         return new Promise((res,rej)=>{
             twitterClient.get('friends/list', {user_id,count}, function(err, data, response) {
                 if(err) rej(err)
@@ -13,7 +13,7 @@ class Twitter {
         })
     }
 
-    async getFriendsOfUser(user_id, count) {
+    getFriendsOfUser(user_id, count) {
         return new Promise((res,rej) => {
             let vm = this
             this.getFriends(user_id, count).then((user) => {
@@ -43,14 +43,78 @@ class Twitter {
         })
     }
 
-    async getFriendsList(user_id, count, next) {
+    getFriendsList(user_id, next) {
         return new Promise((res,rej)=>{
-            twitterClient.get('users/lookup', {count, user_id}, function(err, data, response) {
+            twitterClient.get('users/lookup', {user_id}, function(err, data, response) {
                 if(err) rej(err)
                 return res(data)
             })
         })
     }
+
+
+
+    /////// get current friends of user 
+    ///////// loop through each friend
+    /////////// if new friend, save in db
+    //////////// if old friend next step (update details)
+    ///////////// get all current friend of user (save in db with created at)
+
+    async performFriendsUpdate(user_id, count, next) {
+        try {
+            let vm = this
+            let user = await this.getFriends(user_id, count)
+            let allFriends = []
+            async.forEach(user.users, async (currentUser,cb) => {
+                let dbUser = await User.find({id_stry:currentUser.id_str}).exec()
+                console.log(dbUser)
+                let currentFriend, following
+                following = await this.getFriends(currentUser.id_str, 50)
+                let newids = following.users.map((el) => el.id_str)
+                if(!dbUser.length) {
+                    let newUser = {...currentUser,friends:newids,isFriend:true,newFriends:true}
+                    currentFriend =  await User.create(newUser)
+
+                }else {
+                    let oldids = {}, newFriends = false
+                    for(let i=0; i<dbUser.friends.length; i++) {
+                        if(!oldids[dbUser.friends[i]]) {
+                            oldids[dbUser.friends[i]] = true
+                        }
+                    }
+                    for(let i=0; i<newids.length; i++) {
+                        if(!oldids[newids[i]]) {
+                            newFriends = true
+                        }
+                    }
+                    let allids = [...newids, ...dbUser.friends]
+                    currentFriend = await User.findOneAndUpdate({id_str:currentUser.id_str}, {screen_name:currentUser.screen_name, friends:allids}, {new:true})
+                    
+                }
+
+            },(err)=>{
+                if(err) return next(err)
+                return allFriends
+            })
+                
+        }catch(e) {
+            next(e)
+        }
+    }
+
+    async getPageData() {
+        const Users = await User.find({})
+        let sendUsers = []
+        for(let i=0; i<Users.length; i++) {
+            const friends = await this.getFriendsList(Users[i].friends)
+            let update = {...Users[i]._doc, friends_info: friends}
+            sendUsers.push(update)               
+        }
+        return sendUsers
+       
+        
+    }
+
 
 
 }
--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: String
+    screen_name: String,
+    id_str: String,
+    isFriend: { type: Boolean, default: false },
+    friends: Array,
+    newFriends: { type: Boolean, default: false }
 });
 
 const User = mongoose.model('User', UserSchema)
