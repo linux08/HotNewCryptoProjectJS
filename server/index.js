ed6fb8060ae060a27cffc6dd77421d2891d26077
linux08 <abimbola130@gmail.com>
2021-08-07 18:50:45 +0100

    chore: more update

--- server/index.js
+++ server/index.js
@@ -1,37 +1,33 @@
 require("dotenv").config();
 
-
-var index = require('./routes/index');
-var cors = require('cors');
+let { router } = require("./routes/index");
+let cors = require("cors");
 
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 
-require("./db")
-
-
-
+require("./db");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(cors()); 
+app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
-
-app.use('/api', index);
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use("/api", router);
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 app.use(express.static("build"));
-
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,28 +1,16 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const Twitter = require("../controllers/twitter");
 const cron = require("node-cron");
 
 const fs = require("fs");
 const path = require("path");
 
-
 const vcTracking = require("../vc.json");
 
 const vcfollowing = require("../vcfollowing.json");
 
-
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
-
-// Schedule tasks to be run on the server.
-cron.schedule("0 10 * * *", function () {
-  console.log("running a task every minute");
-});
+const { sleep, difference } = require("../utils");
 
 const twitterCrt = new Twitter();
 
@@ -30,16 +18,31 @@ const performOperation = async () => {
   console.log("--------");
   let error = 0;
   for (let i = 0; i < vcTracking.length; i++) {
-     let data = { [vcTracking[i]]: {} };
-     console.log("inside loop", data);
+    let data = { [vcTracking[i]]: {} };
+    console.log("inside loop", data);
     try {
       //Timer to delay from making the twitter api call immediately
-      console.log("start",i);
+      console.log("start", i);
       await sleep(10000);
-      console.log("end",i)
+      console.log("end", i);
       let result = await twitterCrt.getFollowing(vcTracking[i], 10);
-      let data = { [vcTracking[i]]: result };
-      vcfollowing.push(data);
+      let data = {
+        userName: vcTracking[i],
+        data: result,
+        time: Date.now(),
+      };
+      vcfollowing.find((c) => c.userName === vcTracking[i])
+        ? vcfollowing.map((c) => {
+            // get the difference between them
+            // post on TG
+            difference(data, c);
+
+            if (c.userName === vcTracking) {
+              return data;
+            }
+            return c;
+          })
+        : vcfollowing.push(data);
     } catch (err) {
       console.log(err);
       break;
@@ -57,7 +60,11 @@ const performOperation = async () => {
   });
 };
 
-performOperation();
+// Schedule tasks to be run on the server.
+cron.schedule("0 10 * * *", function () {
+  console.log("running a task every minute");
+  performOperation();
+});
 
 router.get("/followers", async (req, res) => {
   try {
@@ -88,7 +95,7 @@ router.get("/friendslist", async (req, res) => {
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
-  var data = null;
+  let data = null;
   try {
     let cb = function (err) {
       if (err) next(err);
@@ -101,4 +108,4 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-module.exports = router;
+module.exports = { router, performOperation };
