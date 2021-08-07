b5f67c283533233f5b025091f819b610b7542dfd
linux08 <abimbola130@gmail.com>
2021-08-07 11:31:00 +0100

    chore: added timestamp

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-    screen_name: String,
-    id_str: String,
-    isFriend: { type: Boolean, default: false },
-    friends: Array,
-    newFriends: { type: Boolean, default: false }
-});
+const UserSchema = new mongoose.Schema(
+    {
+        screen_name: String,
+        id_str: String,
+        isFriend: { type: Boolean, default: false },
+        friends: Array,
+        newFriends: { type: Boolean, default: false },
+    },
+    { timestamps: { createdAt: "created_at",updatedAt: "updated_at" }}
+);
 
 const User = mongoose.model('User', UserSchema)
 
