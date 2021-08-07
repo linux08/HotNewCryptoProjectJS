6e826ff48fbd21aa5c8be42b55a73c218335707a
linux08 <abimbola130@gmail.com>
2021-08-07 18:08:44 +0100

    chore: integration with txt file

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,22 @@ var router = express.Router();
 const Twitter = require("../controllers/twitter");
 const cron = require("node-cron");
 
+const fs = require("fs");
+const path = require("path");
+
+
+const vcTracking = require("../vc.json");
+
+const vcfollowing = require("../vcfollowing.json");
+
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+
 // Schedule tasks to be run on the server.
 cron.schedule("0 10 * * *", function () {
   console.log("running a task every minute");
@@ -10,15 +26,47 @@ cron.schedule("0 10 * * *", function () {
 
 const twitterCrt = new Twitter();
 
+const performOperation = async () => {
+  console.log("--------");
+  let error = 0;
+  for (let i = 0; i < vcTracking.length; i++) {
+     let data = { [vcTracking[i]]: {} };
+     console.log("inside loop", data);
+    try {
+      //Timer to delay from making the twitter api call immediately
+      console.log("start",i);
+      await sleep(10000);
+      console.log("end",i)
+      let result = await twitterCrt.getFollowing(vcTracking[i], 10);
+      let data = { [vcTracking[i]]: result };
+      vcfollowing.push(data);
+    } catch (err) {
+      console.log(err);
+      break;
+    }
+  }
+
+  const jsonString = JSON.stringify(vcfollowing);
+
+  fs.writeFile(path.join(__dirname, "../vcfollowing.json"), jsonString, (err) => {
+    if (err) {
+      console.log("Error writing file", err);
+    } else {
+      console.log("Successfully wrote file");
+    }
+  });
+};
+
+performOperation();
+
 router.get("/followers", async (req, res) => {
   try {
-    const resp =  await twitterCrt.getFollowers(req.body.userId);
+    const resp = await twitterCrt.getFollowers(req.body.userId);
     res.send(resp);
   } catch (err) {
     res.status(500).send(err);
   }
 });
-
 
 router.get("/following", async (req, res) => {
   try {
@@ -30,12 +78,12 @@ router.get("/following", async (req, res) => {
 });
 
 router.get("/friendslist", async (req, res) => {
-   try {
-     const resp = await twitterCrt.getFriendsOfUser(req.body.userId, req.body.count || 20);
-     res.send(resp);
-   } catch (err) {
-     res.status(500).send(err);
-   }
+  try {
+    const resp = await twitterCrt.getFriendsOfUser(req.body.userId, req.body.count || 20);
+    res.send(resp);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 /* GET home page. */
